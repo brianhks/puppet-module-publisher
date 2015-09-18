66cb46816b33c2f6ff27495a76890862cb6595fb
Brian Hawkins <brianhks1@gmail.com>
2015-09-18 14:37:04 -0600

    Added check for missing metadata.json

--- publish_modules.sh
+++ publish_modules.sh
@@ -4,7 +4,17 @@
 #https://support.sonatype.com/entries/22189106-How-can-I-programatically-upload-an-artifact-into-Nexus-
 
 #List and parse module versions
-librarian-puppet show
+MODULES=`librarian-puppet show`
+
+#Check for metadata.json
+while read -r line; do
+	module=`echo $line | cut -d ' ' -f 1 | cut -d '-' -f 2`
+	if [ ! -e "modules/$module/metadata.json" ]
+	then
+		echo >&2 "Module $module is missing a metadata.json file, you must add one before uploading."
+		exit 1;
+	fi
+done <<< "$MODULES"
 
 #Verify each module has a metadata.json
 
